c05b063ed83bb02c7d83040a2a85d972945bc90e
parshas <shachar.parshani@gamil.com>
2023-04-18 16:32:20 +0300

    automaton update

--- Automaton.py
+++ Automaton.py
@@ -1,17 +1,90 @@
 import random
-class automaton:
-    def _init_(self, p, l, pros1, pros2, pros3, pros4):
+import Person
+import time
+
+
+class Automaton:
+    def _init_(self, p, l, pros1, pros2, pros3, pros4, endGen):
         self.p = p
         self.l = l
-        self.pro = pro  # probabilities of s
-        self.matrix = [[None for j in range(100)] for i in range(100)] # create a 2D array
+        self.generation = 0
+        self.endGen = endGen
+        self.pros1 = pros1  # probabilities of s1
+        self.pros2 = pros2  # probabilities of s2
+        self.pros3 = pros3  # probabilities of s3
+        self.pros4 = pros4  # probabilities of s4
+        self.matrix = [[None for j in range(100)] for i in range(100)]  # create a 2D array
+
     def create_matrix(self):
         # Define the options and their probabilities
         options = [1, 2, 3, 4]
-        probabilities = [0.1, 0.2, 0.3, 0.4]
+        probabilities = [self.pros1, self.pros2, self.pros3, self.pros4]
 
         # Generate a random number from the options with specified probabilities
-        random_number = random.choices(options, probabilities)[0]
+        random_num = random.choices(options, probabilities)[0]
         for i in range(100):
             for j in range(100):
-                self.matrix[i][j] = Person()
+                self.matrix[i][j] = Person(random_num)
+
+    def print_array(self):
+        for i in range(100):
+            for j in range(100):
+                if self.matrix[i][j].gen == -1:
+                    print("-\t")
+                else:
+                    print("*\t")
+            print("\n")
+
+    def run(self):
+        # random starter
+        row = random.randint(0, 99)
+        col = random.randint(0, 99)
+        self.matrix[row][col].gen = 0
+        self.print_array()
+        time.sleep(1)  # waits for 1 second
+
+        self.matrix[row][col].prevent = 1
+        self.matrix[row + 1][col].gen = 1
+        self.matrix[row - 1][col].gen = 1
+        self.matrix[row][col + 1].gen = 1
+        self.matrix[row][col - 1].gen = 1
+        self.matrix[row + 1][col].count_get = 1
+        self.matrix[row - 1][col].count_get = 1
+        self.matrix[row][col + 1].count_get = 1
+        self.matrix[row][col - 1].count_get = 1
+        self.print_array()
+        time.sleep(1)  # waits for 1 second
+
+        self.generation = 2
+        while self.generation <= self.endGen:
+            for i in range(100):  # spreading the rumor
+                for j in range(100):
+                    if self.matrix[i][j].gen == self.generation - 1 & self.matrix[i][j].prevent != 0:
+                        # got the rumor in the last generation and not prevented
+                        s = self.matrix[i][j].s if self.matrix[i][j].pre_count_get < 2 else self.matrix[i][j].s - 1
+                        pro_spread = Person.probability_to_spread(s)
+                        outcomes = [0, 1]
+                        weights = [1 - pro_spread, pro_spread]
+                        is_spread = random.choices(outcomes, weights)[0]
+                        if is_spread == 1:
+                            self.matrix[i][j].prevent = self.generation
+                            self.matrix[i + 1][j].gen = self.generation
+                            self.matrix[i - 1][j].gen = self.generation
+                            self.matrix[i][j + 1].gen = self.generation
+                            self.matrix[i][j - 1].gen = self.generation
+                            self.matrix[i + 1][j].count_get += 1
+                            self.matrix[i - 1][j].count_get += 1
+                            self.matrix[i][j + 1].count_get += 1
+                            self.matrix[i][j - 1].count_get += 1
+
+            for i in range(100):
+                for j in range(100):
+                    self.matrix[i][j].pre_count_get = self.matrix[i][j].count_get
+                    self.matrix[i][j].count_get = 0
+                    if (self.generation - self.matrix[i][j].prevent) > self.l:
+                        self.matrix[i][j].prevent = 0
+            self.generation += 1  # update the generation
+            self.print_array()
+            time.sleep(1)  # waits for 1 second
+
+
--- Person.py
+++ Person.py
@@ -1,4 +1,19 @@
+def probability_to_spread(s_level):  # Probability of spreading the rumor
+    if s_level == 1 | s_level == 0:  # 0 for the case that the level of skepticism temporarily decreased by one
+        return 1
+    if s_level == 2:
+        return 2/3
+    if s_level == 3:
+        return 1/3
+    if s_level == 4:
+        return 0
+
+
 class Person:
     def _init_(self, s):
         self.s = s  # level of skepticism. valid - 1,2,3,4
-        self.gen = -1  # A generation of hearing. -1 no hearing
+        self.gen = -1  # the generation of hearing the rumor. -1 no hearing
+        self.count_get = 0  # count how many times this person got the rumor in the current generation
+        self.pre_count_get = 0  # like count_get, for the previous generation
+        self.prevent = 0  # 0 if the person is not prevented from spreading the rumor, otherwise -
+        # which generation the person spreaded the rumor
